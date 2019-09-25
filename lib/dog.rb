206816e0d72d2bc7b94351189a630829903e19a4
Michael Lynch <michael@michaelrlynch.com>
2019-09-25 18:02:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,13 @@
 class Dog
   attr_reader :name 
-  attr_accessor :mood
+  attr_accessor :mood, :owner
+  
+  @@all = []
   
   def initialize(name)
     @name = name
     @mood = "Nervous"
+    @owner = owner
+    @@all << self
   end
 end
