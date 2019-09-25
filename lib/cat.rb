b2ea8388ad6ee073469dbbc46d456c0b97731774
Michael Lynch <michael@michaelrlynch.com>
2019-09-25 18:01:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
   attr_reader :name 
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   
   def initialize(name)
     @name = name
     @mood = "Nervous"
+    @owner = owner
   end
   
 end
