e61ac8b43b89028e700c2a85d1a763d03e19201e
Michael Lynch <michael@michaelrlynch.com>
2019-09-25 17:58:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,7 @@ class Owner
   
   @@owners = []
   
-  def initialize(species, @dogs => [], @cats => [])
+  def initialize(species, :dogs => [], :cats => [])
     @species = species
     @name = name
     @@owners << self
